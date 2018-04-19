fb72a8454bf781a2eb1f70011e694e4002e88e80
Kristoffer Farstad <kristofferlfarstad@gmail.com>
2018-04-19 13:15:11 +0200

    saveload function
    save
    load
    write to file
    read from file

---
+++ Kristoffer saveloadrestart/com/choppyfloppy/Enemy.java
@@ -0,0 +1,41 @@
+package com.choppyfloppy;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
+
+public class Enemy extends GameObject {
+
+    private boolean flipImage = false;
+
+    public Enemy(ImageView imageView, Vector2D position, Rectangle bounds){
+        super(imageView, position, bounds);
+    }
+
+    public void update(Enemy enemy, Player player) {
+        final double velocity = 1;
+
+        if(player.getPosition().getX() > enemy.getPosition().getX()){
+            enemy.getPosition().addX(velocity);
+        }else if(player.getPosition().getX() < enemy.getPosition().getX()){
+            enemy.getPosition().addX(-velocity);
+            flipImage = true;
+        }
+
+        if(player.getPosition().getY() > enemy.getPosition().getY()){
+            enemy.getPosition().addY(velocity);
+        }else if(player.getPosition().getY() < enemy.getPosition().getY()){
+            enemy.getPosition().addY(-velocity);
+        }
+    }
+
+    @Override
+    public void draw(GraphicsContext gc) {
+        if(flipImage){
+            gc.drawImage(getImageView().getImage(), getPosition().getX() + getBounds().getWidth(), getPosition().getY(), -getBounds().getWidth(), getBounds().getHeight());
+            flipImage = false;
+        }else
+
+        gc.drawImage(getImageView().getImage(), getPosition().getX(), getPosition().getY());
+    }
+}
---
+++ Kristoffer saveloadrestart/com/choppyfloppy/saveload/writetosavefile.java
@@ -0,0 +1,9 @@
+package com.choppyfloppy.saveload;
+
+public class writetosavefile {
+
+   // Lag egen klasse for datafield.
+    //Legg inn datafelt for navn, level og score
+
+
+}
