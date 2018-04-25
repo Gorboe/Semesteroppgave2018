b1e86a9d410d9a4fa45193a602ec61a8dce1ca28
Kristoffer Farstad <kristofferlfarstad@gmail.com>
2018-04-25 13:34:58 +0200

    added save

--- Innlevering/com/choppyfloppy/saveload/createsavefile.java
+++ Innlevering/com/choppyfloppy/saveload/createsavefile.java
@@ -5,9 +5,17 @@ import java.io.PrintWriter;
 
 public class createsavefile {
 
+    static int playerScore = 14;
+    static int currentLevel = 4;
+
+    //legg til filename; saveFile(String filename){
+    //Lager savefile
+
     public static void saveFile(){
         try {
             PrintWriter saveFile = new PrintWriter("saveFolder/save.txt", "UTF-8");
+            saveFile.println(playerScore);
+            saveFile.println(currentLevel);
             saveFile.close();
         }catch (IOException e){
             e.printStackTrace();
